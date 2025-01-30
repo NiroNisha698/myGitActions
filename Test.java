21af096edbdbaadb968f01efed07173c8d354e72
NiroNisha698 <52610136+NiroNisha698@users.noreply.github.com>
2025-01-30 12:08:56 +0530

    Update h Test.java

--- Test.java
+++ Test.java
@@ -1,15 +1,10 @@
-package mypackage; // If using a package
-
-/**
- * Main class for the CI/CD testing application.
- */
 class Test {
-    // Private constructor to prevent instantiation
     private Test() {
-        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+        // Prevent instantiation
     }
 
     public static void main(final String[] args) {
         System.out.println("My CI_CD testing app10 Aurena!!");
     }
 }
+
