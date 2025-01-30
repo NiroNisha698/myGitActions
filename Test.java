89428fc6eb926fc20e29390c82993a4f5fb5097c
NiroNisha698 <52610136+NiroNisha698@users.noreply.github.com>
2025-01-30 11:59:22 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,5 @@
-class Test
-{
-    public static void main(String[] args)
-    {
+class Test {
+    public static void main(String[] args) {
         System.out.println("My CI/CD testing app10 Aurena!!");
     }
 }
