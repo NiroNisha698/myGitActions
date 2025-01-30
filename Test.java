d89e2f3860be2c5837a34325e5f314753ffec049
NiroNisha698 <52610136+NiroNisha698@users.noreply.github.com>
2025-01-30 11:41:20 +0530

    Syntax error correction Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ class Test
 {
     public static void main(String[] args)
     {
-        System.out.println("My CI/CD testing);
+        System.out.println("My CI/CD testing app10 Aurena!!");
     }
 }
